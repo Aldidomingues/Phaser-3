eae15a2b7d3982200587e922fd1fa958b80d14a6
Aldidomingues <aldanaD104@gmail.com>
2022-07-03 18:34:15 -0300

    Actualizacion

---
+++ public/assets/tilemaps/fondo5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="fondo5" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="../images/fondo5.png" width="1920" height="1080"/>
+</tileset>
